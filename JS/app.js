fd5710e3494878a5237e0c5aec8a00b7744ee453
fndeveloper <fasihnasir60@gmail.com>
2025-01-07 15:21:32 +0500

    Updated code with new changes

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,21 @@
 
 
-// window.addEventListener("scroll", function () {
-//   var navbar = document.getElementById("navbar");
-//   if (window.scrollY > 600) {
-//     navbar.classList.add("navfix"); // Add fade-in class
-//   } else {
-//     navbar.classList.remove("navfix"); // Remove fade-in class
-//   }
-// });
+window.addEventListener("scroll", function () {
+    var navunder=document.getElementById("navunder");
+    const scrollableHeight = document.body.scrollHeight - window.innerHeight;
+
+
+    const scrollProgress = (window.scrollY / scrollableHeight) * 100;
+
+
+    
+    navunder.style.width=`${scrollProgress}vw`
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 600) {
+    navbar.classList.add("navfix"); // Add fade-in class
+  } else {
+    navbar.classList.remove("navfix"); // Remove fade-in class
+  }
+});
+
+
