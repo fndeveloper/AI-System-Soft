46a30c9094b5614bcb309106320bab536117edbe
fndeveloper <fasihnasir60@gmail.com>
2025-01-27 20:52:02 +0500

    c

--- JS/app.js
+++ JS/app.js
@@ -2,47 +2,58 @@
 
 window.addEventListener("scroll", function () {
 
-    
-    var navunder=document.getElementById("navunder");
-    const scrollableHeight = document.body.scrollHeight - window.innerHeight;
-    
-    
-    const scrollProgress = (window.scrollY / scrollableHeight) * 98;
 
-    if(scrollProgress >95){
-      document.getElementById("topbtn").innerHTML=`<i class="fa-solid fa-arrow-up"></i>`;
-    }
-    else{
-      this.document.getElementById("topbtn").innerHTML=scrollProgress.toFixed(0)+"%";
+  var navunder = document.getElementById("navunder");
 
-    }
-    navunder.style.width=`${scrollProgress}vw`
-    var navbar = document.getElementById("navbar");
-    var navbar1 = document.getElementById("navbar1");
 
-    if(this.window.scrollY>200){
+  const scrollableHeight = document.body.scrollHeight - window.innerHeight;
 
-        document.getElementById("home").style.transform = "scale(0.950)";
-    }
-    else{
-        document.getElementById("home").style.transform = "scale(1)";
 
-    }
-    if (window.scrollY > 400) {
-    navbar.classList.add("navfix");// Add fade-in class
+  const scrollProgress = (window.scrollY / scrollableHeight) * 98;
+
+  if (scrollProgress > 95) {
+    document.getElementById("topbtn").innerHTML = `<i class="fa-solid fa-arrow-up"></i>`;
+
+
+
+  }
+  else {
+    this.document.getElementById("topbtn").innerHTML = scrollProgress.toFixed(0) + "%";
+
+  }
+  navunder.style.width = `${scrollProgress}vw`
+  var navbar = document.getElementById("navbar");
+  var navbar1 = document.getElementById("navbar1");
+  var iconf = document.querySelectorAll(".btn-red");
+
+
+  var navlinkd = this.document.querySelectorAll(".nav-item>.nav-link")
+
+  if (window.scrollY > 400) {
+    navbar.classList.add("navfix");
+
+    iconf.forEach((e) => {
+      e.classList.add("icon-fixeds")
+    })
+    navlinkd.forEach((e) => {
+
+
+      e.classList.add("nav-sec-col")
+    })
   } else {
-    navbar.classList.remove("navfix"); // Remove fade-in class
+    iconf.forEach((e) => {
+      e.classList.remove("icon-fixeds")
+    })
+    navbar.classList.remove("navfix");
+    navlinkd.forEach((e) => {
+
+
+      e.classList.remove("nav-sec-col")
+    })
   }
 });
 
-var contactform=document.getElementById("contact-form")
-
-const cursorPoint = document.getElementById('cursorPoint');
-document.addEventListener('mousemove', (event) => {
-
- 
-  cursorPoint.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
-});
+;
 
 
 // COUNTRY CODE
