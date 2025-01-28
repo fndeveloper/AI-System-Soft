1967c489a0ffe9b252aa5a17aa7b3dcaf2220948
fndeveloper <fasihnasir60@gmail.com>
2025-01-28 17:42:16 +0500

    c

--- JS/app.js
+++ JS/app.js
@@ -67,3 +67,20 @@ inputs.forEach(input => {
     });
 });
 // COUNTRY CODE
+
+// ACCORDIAN
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+} 
+// ACCORDIAN
