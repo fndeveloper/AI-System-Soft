226ab1f1cf3ea7a009544912a5e77a248d63373f
fndeveloper <fasihnasir60@gmail.com>
2025-01-30 19:15:37 +0500

    c

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,12 @@
 
 
 window.addEventListener("scroll", function () {
-
-
   var navunder = document.getElementById("navunder");
-
-
   const scrollableHeight = document.body.scrollHeight - window.innerHeight;
-
-
   const scrollProgress = (window.scrollY / scrollableHeight) * 98;
-
   if (scrollProgress > 95) {
     document.getElementById("topbtn").innerHTML = `<i class="fa-solid fa-arrow-up"></i>`;
-
-
-
-  }
+}
   else {
     this.document.getElementById("topbtn").innerHTML = scrollProgress.toFixed(0) + "%";
 
@@ -31,16 +21,16 @@ window.addEventListener("scroll", function () {
 
   if (window.scrollY > 400) {
     navbar.classList.add("navfix");
-
+this.document.getElementById("logo").src="Assets/images/Logo/Ai-SS-Logo.jpg"
     iconf.forEach((e) => {
       e.classList.add("icon-fixeds")
     })
     navlinkd.forEach((e) => {
-
-
       e.classList.add("nav-sec-col")
     })
   } else {
+this.document.getElementById("logo").src="Assets/images/Logo/light_logo.png"
+
     iconf.forEach((e) => {
       e.classList.remove("icon-fixeds")
     })
@@ -60,11 +50,11 @@ window.addEventListener("scroll", function () {
 const inputs = document.querySelectorAll(".phonenum");
 
 inputs.forEach(input => {
-    window.intlTelInput(input, {
-        initialCountry: "us",
-        separateDialCode: true,
-        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js", // For formatting
-    });
+  window.intlTelInput(input, {
+    initialCountry: "us",
+    separateDialCode: true,
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js", // For formatting
+  });
 });
 // COUNTRY CODE
 
@@ -73,14 +63,14 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
-} 
+}
 // ACCORDIAN
