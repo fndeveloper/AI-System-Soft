12433144613e4c8922396d41831efee595a21554
fndeveloper <fasihnasir60@gmail.com>
2025-02-01 19:44:25 +0500

    C

--- JS/app.js
+++ JS/app.js
@@ -77,3 +77,39 @@ for (i = 0; i < acc.length; i++) {
 // MARQUEE START
 
 // MARQUEE END
+
+// SWIPER HOME JS 
+const sliderWrapper = document.getElementById('sliderWrapper');
+const slides = document.querySelectorAll('.slider-item');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentIndex = 0;
+
+
+function showSlide(index) {
+
+  if (index >= slides.length) {
+    currentIndex = 0; 
+  } else if (index < 0) {
+    currentIndex = slides.length - 1; 
+  } else {
+    currentIndex = index;
+  }
+
+
+  sliderWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+
+nextBtn.addEventListener('click', () => {
+  showSlide(currentIndex + 1); 
+});
+
+prevBtn.addEventListener('click', () => {
+  showSlide(currentIndex - 1); 
+});
+
+
+showSlide(currentIndex);
+// SWIPER HOME JS 
